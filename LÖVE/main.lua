791f9737ae0cc6e30a95bd1d29df96ed9b739731
Reavershark <jonasty1999@gmail.com>
2017-01-02 00:40:56 +0100

    Window & Font setup

--- LÖVE/main.lua
+++ LÖVE/main.lua
@@ -1,11 +1,16 @@
 function love.load()
+	--Window setup
+	love.window.setMode(800, 600, {resizable=true, minwidth=800, minheight=600})
+	icon = love.image.newImageData("sprites/windowIcon.png")
+	love.window.setIcon(icon)
+	love.window.setTitle("Dank game")
 
+	--Load font in RAM
+	font = love.graphics.newFont("fonts/PressStart2P.ttf")
+	love.graphics.setFont(font)
 end
 
 function love.draw()
-
-end
-
-function love.update()
-
+	--Test draw
+	love.graphics.print("Hello world!\nYour display calls itself '" .. love.window.getDisplayName(1) .. "'\nSooooo...\nYea.", 0, 10)
 end
