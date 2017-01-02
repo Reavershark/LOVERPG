288d5b8bfca36224796f17e5e5aca5ba4f559989
Reavershark <jonasty1999@gmail.com>
2017-01-02 12:16:46 +0100

    HUMP Camera module

--- LÖVE/main.lua
+++ LÖVE/main.lua
@@ -1,9 +1,15 @@
+Camera = require "camera"
+
 function love.load()
 	--Window setup
 	love.window.setMode(800, 600, {resizable=true, minwidth=800, minheight=600})
 	icon = love.image.newImageData("sprites/windowIcon.png")
 	love.window.setIcon(icon)
 	love.window.setTitle("Dank game")
+	
+	--Camera setup
+	cam = Camera.new()
+	cam:lockPosition(love.graphics.getWidth()/2, love.graphics.getHeight()/2)
 
 	--Load font in RAM
 	font = love.graphics.newFont("fonts/PressStart2P.ttf")
@@ -11,6 +17,8 @@ function love.load()
 end
 
 function love.draw()
+	cam:attach()
 	--Test draw
 	love.graphics.print("Hello world!\nYour display calls itself '" .. love.window.getDisplayName(1) .. "'\nSooooo...\nYea.", 0, 10)
+	cam:detach()
 end
